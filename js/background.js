9ba3627f5fec2697290162d2396db78f2878a0c6
Felix Wolff <felix.stefan.wolff@gmail.com>
2014-11-05 14:43:28 +0100

    Test für dich

--- js/background.js
+++ js/background.js
@@ -24,6 +24,23 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     case 'onTabStateChange':
       changeStateIconTo(request.state)
       return true
+
+    case 'checkIfContained':
+      request.open("GET", "http://192.168.42.38:8003/WebPlugin/checkUrl.xsjs?url=de.amulette-kelten.www:http/Amulette-der-Kelten-Produktseiten-01/Amulette-der-Kelten-Weise-Goettin-Anhaenger-WJ-P645s.html", true)
+      request.setRequestHeader('Authorization', 'Basic U01BMTQxNTpQb3Bjb3JuNTQ=')
+      request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+          if (request.status == 200) {
+            console.log('Website inside the database!')
+            state = 'ready'
+            chrome.runtime.sendMessage({type : "onTabStateChange", state : state})
+          } else {
+            console.log('Website is not inside!')
+          }
+        }
+      }
+      request.send();
+      return true;
   }
 })
 
