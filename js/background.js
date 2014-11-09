0a6c57f1b4434b52c10a4058672d41613f8f2a57
Felix Wolff <felix.stefan.wolff@gmail.com>
2014-11-09 16:17:43 +0100

    Rework background and content scripts for simpler communication and clear seperation of concerns

--- js/background.js
+++ js/background.js
@@ -1,53 +1,69 @@
-/*chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.type == "pluginState:ready") {
-    chrome.browserAction.setIcon({details : 'img/icon_ready.png'}, sender.tab.id)
-  }
-})*/
+// ---- global vars -----------------------------
+var tabStates = {}
+var currentTabId = null
+var dbLocation = "http://192.168.42.38:8003/WebPlugin/checkUrl.xsjs"
+//var origin     = reverseDomainName(document.location.origin.replace('http://', '')) + ":http" + document.location.pathname
 
-var currentTabState = null
-var currentTabId    = null
-
-// every time the tab changes, we need to check whether the plugin is already
-// active here. For any state, the icon needs to be change accordingly
-chrome.tabs.onActivated.addListener(function(changeInfo) {
-  chrome.tabs.sendMessage(changeInfo.tabId, { type : "getTabState"}, function(response) {
-    currentTabId = changeInfo.tabId
-    changeStateIconTo(response.state)
-  })
-})
-
-// when the page inside a tab changes, the above signal is not sent,
-// but the main script still gets run. So the main script can trigger
-// an icon change by itself as well
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  switch (request.type) {
-    case 'onTabStateChange':
-      changeStateIconTo(request.state)
-      return true
-
-    case 'checkIfContained':
-      var httpRequest = new XMLHttpRequest()
-      request.open("GET", "http://192.168.42.38:8003/WebPlugin/checkUrl.xsjs?url=" + httpRequest.url, true)
-      httpRequest.setRequestHeader('Authorization', 'Basic U01BMTQxNTpQb3Bjb3JuNTQ=')
-      httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState == 4) {
-          if (httpRequest.status == 200) {
-            console.log('Website inside the database!')
-            chrome.runtime.sendMessage({type : "setTabState", state : 'ready'})
-          } else {
-            chrome.runtime.sendMessage({type : "setTabState", state : 'inactive'})
-          }
-        }
-      }
-      httpRequest.send();
-      return true;
-  }
-})
-
-// local functions ------------------------------
+// ---- global functions ------------------------
 function changeStateIconTo(state) {
   var icon = 'img/icon_' + state + '.png'
 
   currentTabState = state
   chrome.browserAction.setIcon({path : icon})
 }
+
+function reverseDomainName(s) {
+  return s.split(".").reverse().join(".")
+}
+
+function acquireTabStateFor(tabId, url) {
+  var httpRequest = new XMLHttpRequest()
+
+  httpRequest.open("GET", dbLocation + "?url=" + url, true)
+  httpRequest.setRequestHeader('Authorization', 'Basic U01BMTQxNTpQb3Bjb3JuNTQ=')
+
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState == 4) {
+      if (httpRequest.status == 200) {
+        console.log('Website inside the database: ' + url)
+        tabStates[tabId].state = 'active'
+      } else {
+        tabStates[tabId].state = 'inactive'
+      }
+    }
+  }
+
+  httpRequest.send()
+}
+
+// ---- logic, bitches --------------------------
+chrome.tabs.onRemoved.addListener(function(removedTabId, removeInfo) {
+  if (removedTabId in tabStates) {
+    delete tabStates[removedTabId]
+  }
+})
+
+// every time a different tab gets highlighted, the displayed information
+// inside the popup needs to be changed
+chrome.tabs.onActivated.addListener(function(changeInfo) {
+  currentTabId = changeInfo.tabId
+
+  if (currentTabId in tabStates) {
+    changeStateIconTo(tabStates[currentTabId].state)
+  } else {
+    tabStates[currentTabId] = {
+      state : 'inactive'
+    }
+  }
+})
+
+
+// when the page inside a tab changes, the content scripts
+// are re-run as well and thus the state information may need to be updated here
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  switch (request.type) {
+    case 'setTabUrl':
+      acquireTabStateFor(sender.tab.id, request.url)
+      return true
+  }
+})
