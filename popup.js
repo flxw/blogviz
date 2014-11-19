19dcdf1513f597fb421df8d52fc9d04ecbd613cd
Felix Wolff <felix.stefan.wolff@gmail.com>
2014-11-19 15:50:28 +0100

    Style interface and display post count for main pages

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 // chrome re-renders the popup every time it gets opened
 chrome.runtime.sendMessage({type: 'getCurrentTabInformation'}, function(tabData) {
-	console.log(tabData)
-	$('#dataArea').text(JSON.stringify(tabData))
+  console.log(tabData)
+  $('#postCount').text(tabData.postCount)
+
+  var categories = []
+
+  for (tag in tabData.tags) {
+    categories.push($('<span>' + tabData.tags[tag] + '</span>'))
+  }
+
+  $('#categoryContainer p').append(categories)
 })
