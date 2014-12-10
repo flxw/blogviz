391fc26c85221d0bd6116f299094d48bd17b95e9
Thomas Goerttler <thomas.goerttler@gmail.com>
2014-12-10 16:11:36 +0100

    Chart data also for post, but data is missing

--- popup/controllers/postcontroller.js
+++ popup/controllers/postcontroller.js
@@ -1,8 +1,38 @@
 angular.module('postPopupPage').controller('PostController', ['$scope', '$location', function($scope, $location) {
 
+  $scope.chartConfig = {
+    title: {
+      text: ''
+    },
+    series: [{
+      type: 'pie',  
+      data: []
+    }],
+    loading: false
+  }
 
   chrome.runtime.sendMessage({type: 'getCurrentTabInformation'}, function(tabData) {
     $scope.postData = tabData
+    
+    $scope.chartConfig.series = [{ type:'pie', name: 'Sentiments'}]
+
+    var chartData = []
+    for (var peter in tabData.sentiments) {
+      chartData.push([peter, tabData.sentiments[peter].count])
+    }
+    
+    chartData.push("Peter",1112)
+    chartData.push("Sabine",2)
+    chartData.push("Thomas",12)
+    chartData.push("Goerttler",2)
+    chartData.push("Felix",12)
+    chartData.push("Spaten",2)
+    chartData.push("Hallo",12)
+    chartData.push("Sibne",2)
+    
+    $scope.chartConfig.series[0].data = chartData
+    
+    
     $scope.$apply()
   })
 }])
